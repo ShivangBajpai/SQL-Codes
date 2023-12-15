14339416e86679d7a6c9c0ee391ba6d74f081333
Shivang Bajpai <153921917+ShivangBajpai@users.noreply.github.com>
2023-12-15 20:31:32 +0530

    Add files via upload

---
+++ SQL 9TH DEC.sql
@@ -0,0 +1,29 @@
+
+Create Table Location (LOCATION_ID int, STREET_ADDRESS varchar(max),Postal_Code varchar(max),City varchar(max),
+ STATE_PROVINCE varchar(max),Country varchar(max))
+
+ Select * from Location
+Insert into Location Values(1000,'1297 Via Cola di Rie','989','Roma','India','IT')
+Insert into Location Values(1100,'93091 Calle della Testa','10934','Venice','India','IT')
+Insert into Location Values(1200,'2017 Shinjuku-ku','1689','Tokyo','Tokyo Prefecture','JP')
+Insert into Location Values(1300,'9450 Kamiya-cho','6823','Hiroshima','India','JP')
+Insert into Location Values(1400,'2014 Jabberwocky Rd','26192','Southlake','Texas','US')
+Insert into Location Values(1500,'2011 Interiors Blvd','99236','South San Francisco','California','US')
+Insert into Location Values(1600,'2007 Zagora St','50090','South Brunswick','New Jersey','US')
+Insert into Location Values(1700,'2004 Charade Rd','98199','Seattle','Washington','US')
+Insert into Location Values(1800,'147 Spadina Ave','M5V 2L7','Toronto','Ontario','CA')
+Insert into Location Values(1900,'6092 Boxwood St','YSW 9T2','Whitehorse','Yukon','CA')
+Insert into Location Values(2000,'40-5-12 Laogianggen','190518','Beijing','India','CN')
+Insert into Location Values(2100,'1298 Vileparle (E)','490231','Bombay','Maharashtra','IN')
+Insert into Location Values(2200,'12-98 Victoria Street','2901','Sydney','New South Wales','AU')
+Insert into Location Values(2300,'198 Clementi North','540198','Singapore','India','SG')
+Insert into Location Values(2400,'8204 Arthur St','6243','London','India','UK')
+Insert into Location Values(2500,'Magdalen Centre, The Oxford Science Park',' OX9 9ZB     ','Oxford','Oxford','UK')
+Insert into Location Values(2600,'9702 Chester Road','9629850293','Stretford','Manchester','UK')
+Insert into Location Values(2700,'Schwanthalerstr. 7031','80925','Munich','Bavaria','DE')
+Insert into Location Values(2800,'Rua Frei Caneca 1360',' 01307-002   ','Sao Paulo','Sao Paulo','BR')
+Insert into Location Values(2900,'20 Rue des Corps-Saints','1730','Geneva','Geneve','CH')
+Insert into Location Values(3000,'Murtenstrasse 921','3095','Bern','BE','CH')
+Insert into Location Values(3100,'Pieter Breughelstraat 837','3029SK','Utrecht','Utrecht','NL')
+Insert into Location Values(3200,'Mariano Escobedo 9991','11932',' Mexico City','Distrito Federal',' MX         ')
+
